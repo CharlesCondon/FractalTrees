1bcbed2c4f8b4ed5872f3354be410d99dd605f3d
Charles Condon <charlescon@live.com>
2023-03-23 21:20:50 -0400

    branch sliders added

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 let angle = 0;
 let angleSlider;
-let branchSlider;
+let branchSliderL;
+let branchSliderR;
 
 function setup() {
   var initCanvas = createCanvas(windowWidth, windowHeight);
   angleSlider = createSlider(0, TWO_PI, PI / 4, 0.01);
-  branchSlider = createSlider(0, .73, 0, .01);
+  branchSliderL = createSlider(0, .73, 0, .01);
+  branchSliderR = createSlider(0, .73, 0, .01);
   initCanvas.parent("background");
   angleSlider.parent("slide");
-  branchSlider.parent("branches");
+  branchSliderL.parent("branches");
+  branchSliderR.parent("branches");
 }
 
 function draw() {
   background(0);
   angle = angleSlider.value();
-  branchNum = branchSlider.value();
+  branchNumL = branchSliderL.value();
+  branchNumR = branchSliderR.value();
   stroke(255);
   strokeWeight(2);
   translate(width * 0.47, height * 0.92);
@@ -27,11 +31,11 @@ function branch(len) {
   if (len > 4) {
     push();
     rotate(angle);
-    branch(len * branchNum);
+    branch(len * branchNumL);
     pop();
     push();
     rotate(-angle);
-    branch(len * branchNum);
+    branch(len * branchNumR);
     pop();
   }
 }
