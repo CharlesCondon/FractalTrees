a63bb0c5142123169e54caaef185ffebefeea068
Charles Condon <charlescon@live.com>
2023-03-27 12:31:39 -0400

    random generation added

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ let angle = 0;
 let angleSlider;
 let branchSliderL;
 let branchSliderR;
+let randoL = (Math.random() * .73);
+let randoR = (Math.random() * .73);
 
 function setup() {
   var initCanvas = createCanvas(windowWidth, windowHeight);
   angleSlider = createSlider(0, TWO_PI, PI / 4, 0.01);
-  branchSliderL = createSlider(0, .73, 0, .01);
-  branchSliderR = createSlider(0, .73, 0, .01);
+  branchSliderL = createSlider(0, .73, randoL, .01);
+  branchSliderR = createSlider(0, .73, randoR, .01);
   initCanvas.parent("backgroundCont");
   angleSlider.parent("slide");
   branchSliderL.parent("branchesL");
   branchSliderR.parent("branchesR");
-  console.log(windowHeight);
 }
 
 function draw() {
